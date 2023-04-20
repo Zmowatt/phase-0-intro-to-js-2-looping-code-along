9198e921696f0ea6bcdd8a0cdbdf592e8929e7ef
Zachary Mowatt <zamowatt@gmail.com>
2023-04-20 14:53:09 -0400

    Looping lab incomplete

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-// Code your solutions in this file
+
+let names = ['Guadalupe', 'Ollie', 'Aki'];
+
+function writeCards(names) {
+    let i = 0;
+    while (i < names.length) {
+        console.log(`Thank you, ${names[i]}, for the wonderful surprise gift!`);
+        i++;
+    }
+
+    return names;
+}
+
+writeCards(names);
+
+
+
+
+function countDown(){
+    let num = 10
+    while (num >= 0) {
+        console.log(--num);
+    }
+    return num;
+}
+
+countDown();
